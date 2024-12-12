7315769c01cda4e79f6d43bc7ace749c642c9fb0
dadothetato <dle32843@gmail.com>
2024-12-13 10:24:22 +1100

    Changed to use the superhero composable functions.

--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperheroAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SuperheroApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuperheroAppTheme {
-        Greeting("Android")
     }
 }
